0013001f22fe52e498f2b66d49a7c4d7e8aaecb8
ronak-mistry-cognoai <ronak.mistry@getcogno.ai>
2021-10-24 23:21:54 +0530

    Updated the code

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Review
+from .models import *
 # Register your models here.
 
+admin.site.register(Review)
+admin.site.register(Search)
+admin.site.register(Review)
 admin.site.register(Review)
